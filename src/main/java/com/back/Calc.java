3cba6054532c4d5faea31453d82b88e1b2ae990b
jjun012 <duwns2057@naver.com>
2026-04-22 11:20:21 +0900

    10 - 20 + 30 = 20, green

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -1,27 +1,44 @@
 package com.back;
 
-
-
 public class Calc {
 
     public static int run(String expression) {
-        String[] expressionBites;
-        int result = 0;
-        if (expression.contains(" + ")) {
-            expressionBites = expression.split(" \\+ ");
-            for (int i = 0; i < expressionBites.length; i++) {
-                result +=Integer.parseInt(expressionBites[i]);
-            }
+        expression = expression.replaceAll(" ","");
 
-            return result;
-        }
-        else if (expression.contains(" - ")) {
-            expressionBites = expression.split(" \\- ");
-            int num1 = Integer.parseInt(expressionBites[0]);
-            int num2 = Integer.parseInt(expressionBites[1]);
+        int plus = expression.lastIndexOf("+");
+        int minus = expression.lastIndexOf("-");
 
-            return num1 - num2;
+        if (plus != -1 && plus > minus) {
+            String left = expression.substring(0, plus);
+            String right = expression.substring(plus + 1);
+            return run(left) + Integer.parseInt(right);
         }
-        return 0;
+
+        if  (minus != -1 && plus < minus) {
+            String left = expression.substring(0, minus);
+            String right = expression.substring(minus + 1);
+            return run(left) - Integer.parseInt(right);
+        }
+        return Integer.parseInt(expression);
     }
+
+//        String[] expressionBites;
+//        int result = 0;
+//        if (expression.contains(" + ")) {
+//            expressionBites = expression.split(" \\+ ");
+//            for (int i = 0; i < expressionBites.length; i++) {
+//                result +=Integer.parseInt(expressionBites[i]);
+//            }
+//
+//            return result;
+//        }
+//        else if (expression.contains(" - ")) {
+//            expressionBites = expression.split(" \\- ");
+//            int num1 = Integer.parseInt(expressionBites[0]);
+//            int num2 = Integer.parseInt(expressionBites[1]);
+//
+//            return num1 - num2;
+//        }
+//        return 0;
+//    }
 }
