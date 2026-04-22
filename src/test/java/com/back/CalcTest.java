9e9209372777e0986eb03a799e48e7172f9d7f23
jjun012 <duwns2057@naver.com>
2026-04-22 11:25:47 +0900

    10 * 10 = 100, red

--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -67,7 +67,10 @@ public class CalcTest {
     void t12() {
         assertThat(Calc.run("10 * 10")).isEqualTo(100);
     }
-
-
+    @Test
+    @DisplayName("10 * -10 == -100")
+    void t13() {
+        assertThat(Calc.run("10 * -10")).isEqualTo(-100);
+    }
 
 }
