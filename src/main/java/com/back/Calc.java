1207235474875b36c9fda8cebfc69ddc1ddfa282
jjun012 <duwns2057@naver.com>
2026-04-22 10:41:36 +0900

    2 - 1 = 1, green

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -1,12 +1,26 @@
 package com.back;
 
+
+
 public class Calc {
+
     public static int run(String expression) {
-        String[] expressionBites = expression.split(" \\+ ");
+        String[] expressionBites;
 
-        int num1=Integer.parseInt(expressionBites[0]);
-        int num2=Integer.parseInt(expressionBites[1]);
+        if (expression.contains(" + ")) {
+            expressionBites = expression.split(" \\+ ");
+            int num1 = Integer.parseInt(expressionBites[0]);
+            int num2 = Integer.parseInt(expressionBites[1]);
 
-        return num1+num2;
+            return num1 + num2;
+        }
+        else if (expression.contains(" - ")) {
+            expressionBites = expression.split(" \\- ");
+            int num1 = Integer.parseInt(expressionBites[0]);
+            int num2 = Integer.parseInt(expressionBites[1]);
+
+            return num1 - num2;
+        }
+        return 0;
     }
 }
