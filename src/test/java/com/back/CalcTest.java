43a959e6c3ccdfdab9e5cb118aef5d539ba262e2
jjun012 <duwns2057@naver.com>
2026-04-22 12:16:35 +0900

    -(10 + 5) = -15, red

--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -122,5 +122,14 @@ public class CalcTest {
     void t23() {
         assertThat(Calc.run("(10 + 20) * 3")).isEqualTo(90);
     }
-
+    @Test
+    @DisplayName("10 + (10 + 5) == 25")
+    void t24() {
+        assertThat(Calc.run("10 + (10 + 5)")).isEqualTo(25);
+    }
+    @Test
+    @DisplayName("-(10 + 5) == -15")
+    void t25() {
+        assertThat(Calc.run("-(10 + 5)")).isEqualTo(-15);
+    }
 }
