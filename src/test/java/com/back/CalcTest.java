b36d9bb86d334ca9b447d7c189194b9822d19b66
jjun012 <duwns2057@naver.com>
2026-04-22 11:32:31 +0900

    10 * 10 * 10 = 1000, green

--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -72,5 +72,9 @@ public class CalcTest {
     void t13() {
         assertThat(Calc.run("10 * -10")).isEqualTo(-100);
     }
-
+    @Test
+    @DisplayName("10 * 10 * 10 == 1000")
+    void t14() {
+        assertThat(Calc.run("10 * 10 * 10")).isEqualTo(1000);
+    }
 }
